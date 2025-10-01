6e33655ffd30f30f018a626ac81aa33cc8672aa2
Teddyburgonde <bandama.teddy@gmail.com>
2025-10-01 21:04:13 +0200

    added logger.info in Balloon.java

--- src/avaj/aircraft/Balloon.java
+++ src/avaj/aircraft/Balloon.java
@@ -1,13 +1,71 @@
 package avaj.aircraft;
 
 public class Balloon extends Aircraft {
-	public Balloon(String name, int longitude, int latitude, int height) {
-		super(name, longitude, latitude, height);
+	
+	public Balloon(String name, Coordinates coordinates) {
+		super(name, coordinates);
 	}
 
+	private static final Logger logger = Logger.getLogger(Balloon.class.getName());
+	protected WeatherTower tower;
+
 	@Override
-	public void	updateWeather(String weather){
-		// - appliquer les règles météo (modifier longitude/latitude/height)
-		// - utiliser Logger.log(...) 
+	public void updateConditions(String weather) {
+		switch (weather) {
+			case "SUN":
+				this.coordinates = new Coordinates(
+					coordinates.getLongitude() + 2,
+					coordinates.getLatitude(),
+					coordinates.getHeight() + 4
+				);
+				logger.info(this + ": Let's enjoy the good weather and take some pics.");
+				break;
+		
+			case "RAIN":
+				this.coordinates = new Coordinates(
+					coordinates.getLongitude(),
+					coordinates.getLatitude(),
+					coordinates.getHeight() - 5
+				);
+				logger.info(this + ": Damn you rain! You messed up my balloon.");
+				break;
+		
+			case "FOG":
+				this.coordinates = new Coordinates(
+					coordinates.getLongitude(),
+					coordinates.getLatitude(),
+					coordinates.getHeight() - 3
+				);
+				logger.info(this + ": Can't see anything! This fog is dangerous.");
+				break;
+		
+			case "SNOW":
+				this.coordinates = new Coordinates(
+					coordinates.getLongitude(),
+					coordinates.getLatitude(),
+					coordinates.getHeight() - 15
+				);
+				Logger.log(this + ": It's snowing. We're gonna crash!");
+				break;
+		}
+
+		if (this.coordinates.getHeight() > 100) {
+			this.coordinates = new Coordinates(
+				this.coordinates.getLongitude(),
+				this.coordinates.getLatitude(),
+				100
+			);
+		}
+		
+		// Si l'avion doit atterrir
+		if (this.coordinates.getHeight() <= 0) {
+			logger.info(this + " landing at [" 
+				+ coordinates.getLongitude() + ", " 
+				+ coordinates.getLatitude() + "].");
+		
+			if (tower != null) {
+				tower.unregister(this);
 	}
+}
+	}	
 }
